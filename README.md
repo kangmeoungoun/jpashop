396be51a7946ee3fe748b29f85ffffa2102cd4cf
kangmeoungoun <kes98202@naver.com>
2021-02-02 21:23:36 +0900

    회원 등록 API

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-### 주문 목록 검색,취소
+### API 개발 기본
+#### 회원 등록 API
+###### 절대로 엔티티를 파라미터로 받으면 안된다.
 
-![image](https://user-images.githubusercontent.com/40969203/105046026-ed2d2b80-5aab-11eb-9604-d6a0ddfcb776.png)
-![image](https://user-images.githubusercontent.com/40969203/105046044-f1594900-5aab-11eb-9db7-e71b735cb3a2.png)
-![image](https://user-images.githubusercontent.com/40969203/105046059-f5856680-5aab-11eb-9f8f-e430c9b24745.png)
-![image](https://user-images.githubusercontent.com/40969203/105046073-f9b18400-5aab-11eb-81ee-f62f817a4e3e.png)
-![image](https://user-images.githubusercontent.com/40969203/105046079-fcac7480-5aab-11eb-833d-fcc1bdfa2a52.png)
-![image](https://user-images.githubusercontent.com/40969203/105046087-ff0ece80-5aab-11eb-9fb5-1c6a9e7e29d1.png)
+![image](https://user-images.githubusercontent.com/40969203/106599550-955aee00-659c-11eb-9127-ec47555deca7.png)
+![image](https://user-images.githubusercontent.com/40969203/106599558-9724b180-659c-11eb-8915-73dec71c587b.png)
+![image](https://user-images.githubusercontent.com/40969203/106599585-9f7cec80-659c-11eb-9ad5-ada2f34a91f5.png)
+![image](https://user-images.githubusercontent.com/40969203/106599598-a3107380-659c-11eb-8572-83e4eb5216ea.png)
+![image](https://user-images.githubusercontent.com/40969203/106599612-a6a3fa80-659c-11eb-8936-a955f8ce7b7a.png)
---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,49 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController{
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+    //==점점 버전 업==//
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+        Member member = new Member();
+        member.setName(request.getName());
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest{
+        @NotEmpty(message = "값이 비어 있을수 없습니다.")
+        @Length(max = 10,message = "열자 이상 넘을수 없습니다.")
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse{
+        public CreateMemberResponse(Long id){
+            this.id = id;
+        }
+        private Long id;
+    }
+}
