74316832522132b1d0fde26e95ccec2b762b3add
kangmeoungoun <kes98202@naver.com>
2021-02-03 22:31:09 +0900

    간단한 주문 조회 V2: 엔티티를 DTO로 변환

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 ### API 개발 고급 - 지연로딩과 조회 성능 최적화
-#### 간단한 주문 조회 V1 : 엔티티를 직접 노출
+#### 간단한 주문 조회 V2: 엔티티를 DTO로 변환
 
-![image](https://user-images.githubusercontent.com/40969203/106746568-129e6580-6666-11eb-8b63-ecf6edc1c29c.png)
-![image](https://user-images.githubusercontent.com/40969203/106746586-16ca8300-6666-11eb-9547-d654cc9389c3.png)
-![image](https://user-images.githubusercontent.com/40969203/106746600-192cdd00-6666-11eb-8f2e-3b63491e7e6c.png)
-![image](https://user-images.githubusercontent.com/40969203/106746608-1c27cd80-6666-11eb-8bc2-17d6a8d828fe.png)
-![image](https://user-images.githubusercontent.com/40969203/106746618-1e8a2780-6666-11eb-90cb-bc8fe7cd212f.png)
-![image](https://user-images.githubusercontent.com/40969203/106746630-221dae80-6666-11eb-8f38-75cae8544ff0.png)
+ORDER -> SQL 1번 -> 결과 주문수 2개
+루프가 돌때
+MEMBER 1번
+DELIVERY 1번
+근데 같은 식벽자 일시 한번만 조회하고 그이후는 영속성컨텍스트 1차캐시에서 가져와 쿼리가 나가지 않는다. 
+
+![image](https://user-images.githubusercontent.com/40969203/106753624-40d47300-666f-11eb-8af9-a196fe25d085.png)
+![image](https://user-images.githubusercontent.com/40969203/106753633-4467fa00-666f-11eb-899a-55f2de618e63.png)
--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,21 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 /**
  * xToOne(ManyToOne,OneToOne)
@@ -28,5 +36,37 @@ public class OrderSimpleApiController{
             order.getDelivery().getAddress();
         }
         return all;
+    }
+    @GetMapping("/api/v2/simple-orders")
+    public Result ordersV2(){
+        //ORDER 2개
+        List<SimpleOrderDto> collect = orderRepository.findAllByString(new OrderSearch())
+                                        .stream()
+                                        .map(SimpleOrderDto :: new)
+                                        .collect(toList());
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+
+    }
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime localDateTime;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order){
+            orderId = order.getId();
+            name   = order.getMember().getName();
+            localDateTime = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
     }
 }
